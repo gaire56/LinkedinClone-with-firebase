0056b0ad7eaaab2a06b131c67f2980785ba8c751
Anant gaire <56.gaire@gmail.com>
2021-01-05 08:27:19 +0545

    try

--- src/Feed.js
+++ src/Feed.js
@@ -3,7 +3,7 @@ import './Feed.css';
 import InputOption from './InputOption';
 import CreateIcon from "@material-ui/icons/Create";
 import ImageIcon from "@material-ui/icons/Image";
-import SubscriptionIcon from "@material-ui/icons/Subscriptions";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
 import EventNoteIcon from "@material-ui/icons/EventNote";
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import Post from './Post';
@@ -53,7 +53,7 @@ function Feed() {
                 </div>
                 <div className="feed__inputOptions">
                     <InputOption Icon={ImageIcon} title='Photo' color="#70B5F9" />
-                    <InputOption Icon={SubscriptionIcon} title='Video' color="#E7A33E" />
+                    <InputOption Icon={SubscriptionsIcon} title='Video' color="#E7A33E" />
                     <InputOption Icon={EventNoteIcon} title='Event' color="#C0CBCD" />
                     <InputOption Icon={CalendarViewDayIcon} title='Write article' color="#7FC15E" />
                 </div>
